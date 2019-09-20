29ad75969dd1284361b197df3a0ff6c3e7d0a101
Viktor Bogutskii <info@siteograf.com>
2019-09-19 22:02:30 -0700

    initial

---
+++ test/actions/loginAction.js
@@ -0,0 +1,17 @@
+const { url, user } = require('../constants');
+
+function loginAction(browser){
+  browser.url(url.loginUrl);
+
+  const emailField = $('//input[@name="email"]');
+  const passwordField = $('//input[@name="password"]');
+  const button = $('//button[@type="submit"]');
+
+  emailField.setValue(user.admin.email);
+  passwordField.setValue(user.admin.password);
+
+  button.click();
+  browser.pause(1000);
+}
+
+module.exports = loginAction;
