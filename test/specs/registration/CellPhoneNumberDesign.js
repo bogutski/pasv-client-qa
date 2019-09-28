ef0ecbb4ef6ec3f16a650b0f608f1504260adc44
TatsianaH <ourflsun@gmail.com>
2019-09-28 12:11:30 -0400

    moved all variables

--- test/specs/registration/CellPhoneNumberDesign.js
+++ test/specs/registration/CellPhoneNumberDesign.js
@@ -3,7 +3,24 @@ import { url } from '../examples/constants';
 import { user } from '../examples/user/constants';
 
 const inputFieldPhone = '//input[@name="phone"]';
+const label = '//label[@for="phone"]';
+const descText = '//input[@name="phone"]/following-sibling::small';
 const firstSymbol = '+';
+const expectedBorder = '#ced4da';
+const expectedFontColor = '#495057';
+const expectedFocusBorderColor = '#4d94ff';
+const expectedFocusHighlightColor = '#0052cc';
+const expectedBackgroundColor = '#ffffff';
+const expectedTextAlign = 'start';
+const expectedWeight = 400;
+const expectedFontFamily =
+  '"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif';
+const expectedBorderColorValid = '#24c88b';
+const expectedDescText = 'Format +17770005511 or +380653332244';
+const expectedLabelSize = '17px';
+const expectLabelColor = '#212529';
+const expectedAlign = 'left';
+const expectedDescTextSize = '13.6px';
 
 describe('Register Page - Cell Phone Number input field - Design', () => {
   before(() => {
@@ -17,7 +34,6 @@ describe('Register Page - Cell Phone Number input field - Design', () => {
 
   it('should check border-color', () => {
     const actualBorder = $(inputFieldPhone).getCSSProperty('border-top-color').parsed.hex;
-    const expectedBorder = '#ced4da';
     expect(actualBorder).to.be.equal(expectedBorder);
   });
 });
@@ -30,46 +46,38 @@ describe('Register Page - Cell Phone Number input field when user enters first s
   it('should check font-color', () => {
     $(inputFieldPhone).setValue(firstSymbol);
     const actualFontColor = $(inputFieldPhone).getCSSProperty('color').parsed.hex;
-    const expectedFontColor = '#495057';
     expect(actualFontColor).to.be.equal(expectedFontColor);
   });
 
   it('should check focus border-color', () => {
     const actualFocusBorderColor = $(inputFieldPhone).getCSSProperty('border-color').parsed.hex;
-    const expectedFocusBorderColor = '#4d94ff';
     expect(actualFocusBorderColor).to.be.equal(expectedFocusBorderColor);
   });
 
   it('should check focus highlight color', () => {
     const actualFocusHighlightColor = $(inputFieldPhone).getCSSProperty('box-shadow').parsed.hex;
-    const expectedFocusHighlightColor = '#0052cc';
     expect(actualFocusHighlightColor).to.be.equal(expectedFocusHighlightColor);
   });
 
   it('should check background-color', () => {
     const actualBackgroundColor = $(inputFieldPhone).getCSSProperty('background-color').parsed.hex;
-    const expectedBackgroundColor = '#ffffff';
     expect(actualBackgroundColor).to.be.equal(expectedBackgroundColor);
   });
 
   it('should check text-alignment', () => {
     const actualTextAlign = $(inputFieldPhone).getCSSProperty('text-align').value;
-    const expectedTextAlign = 'start';
     expect(actualTextAlign).to.be.equal(expectedTextAlign);
   });
 
   it('should check font-family', () => {
     const actualfontFamily = $(inputFieldPhone).getCSSProperty('font-family').parsed.string;
-    const expectedfontFamily =
-      '"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif';
-    expect(actualfontFamily).to.be.equal(expectedfontFamily);
+    expect(actualfontFamily).to.be.equal(expectedFontFamily);
   });
 
   it('should check font-weight', () => {
     const actualWeightObj = $(inputFieldPhone).getCSSProperty('font-weight');
     const actualWeight = Object.values(actualWeightObj)[1];
-    const expectedweight = 400;
-    expect(actualWeight).to.be.equal(expectedweight);
+    expect(actualWeight).to.be.equal(expectedWeight);
   });
 });
 
@@ -80,7 +88,6 @@ describe('Register Page - Cell Phone Number input field when the number is valid
     browser.keys('Tab');
     browser.pause(300);
     const actualBorderColorValid = $(inputFieldPhone).getCSSProperty('border-color').parsed.hex;
-    const expectedBorderColorValid = '#24c88b';
     expect(actualBorderColorValid).to.be.equal(expectedBorderColorValid);
   });
 });
@@ -91,40 +98,32 @@ describe('Register Page - Cell phone number Label - Design', () => {
   });
 
   it('should check that the label is displayed.', () => {
-    const actualLabel = $('//label[@for="phone"]').isDisplayed();
-    expect(actualLabel).to.be.true;
+    expect($(label).isDisplayed()).to.be.true;
   });
 
   it('should check font-family', () => {
-    const actualLabelfontFamily = $('//label[@for="phone"]').getCSSProperty('font-family').parsed
-      .string;
-    const expectedLabelfontFamily =
-      '"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif';
-    expect(actualLabelfontFamily).to.be.equal(expectedLabelfontFamily);
+    const actualLabelfontFamily = $(label).getCSSProperty('font-family').parsed.string;
+    expect(actualLabelfontFamily).to.be.equal(expectedFontFamily);
   });
 
   it('should check font-size', () => {
-    const labelSizeObj = $('//label[@for="phone"]').getCSSProperty('font-size').value;
-    const expectedLabelSize = '17px';
+    const labelSizeObj = $(label).getCSSProperty('font-size').value;
     expect(labelSizeObj).to.be.equal(expectedLabelSize);
   });
 
   it('should check font-color', () => {
-    const actualLabelColor = $('//label[@for="phone"]').getCSSProperty('color').parsed.hex;
-    const expectLabelColor = '#212529';
+    const actualLabelColor = $(label).getCSSProperty('color').parsed.hex;
     expect(actualLabelColor).to.be.equal(expectLabelColor);
   });
 
   it('should check font-weight', () => {
-    const actualLabelWeight = $('//label[@for="phone"]').getCSSProperty('font-weight').value;
-    const expectLabelWeight = 400;
-    expect(actualLabelWeight).to.be.equal(expectLabelWeight);
+    const actualLabelWeight = $(label).getCSSProperty('font-weight').value;
+    expect(actualLabelWeight).to.be.equal(expectedWeight);
   });
 
   it('should check text-alignment', () => {
-    const actualLabelAlign = $('//label[@for="phone"]').getCSSProperty('text-align').value;
-    const expectLabelAlign = 'left';
-    expect(actualLabelAlign).to.be.equal(expectLabelAlign);
+    const actualLabelAlign = $(label).getCSSProperty('text-align').value;
+    expect(actualLabelAlign).to.be.equal(expectedAlign);
   });
 });
 
@@ -134,54 +133,37 @@ describe('Register Page - Cell phone number description text - Design', () => {
   });
 
   it('should check that the text is displayed', () => {
-    const actualDescText = $('//input[@name="phone"]/following-sibling::small').isDisplayed();
-    expect(actualDescText).to.be.true;
+    expect($(descText).isDisplayed()).to.be.true;
   });
 
   it('should check that the text is “Format +17770005511 or +380653332244”', () => {
-    const actualDescText = $('//input[@name="phone"]/following-sibling::small').getText();
-    const expectedDescText = 'Format +17770005511 or +380653332244';
+    const actualDescText = $(descText).getText();
     expect(actualDescText).to.be.equal(expectedDescText);
   });
 
   it('should check font-family', () => {
-    const actualDescTextFontFamily = $(
-      '//input[@name="phone"]/following-sibling::small',
-    ).getCSSProperty('font-family').parsed.string;
-    const expectedDescTextFontFamily =
-      '"sf pro display", "sf pro icons", "helvetica neue", helvetica, arial, sans-serif';
-    expect(actualDescTextFontFamily).to.be.equal(expectedDescTextFontFamily);
+    const actualDescTextFontFamily = $(descText).getCSSProperty('font-family').parsed.string;
+    expect(actualDescTextFontFamily).to.be.equal(expectedFontFamily);
   });
 
   it('should check font-size', () => {
-    const actualDescTextSize = $('//input[@name="phone"]/following-sibling::small').getCSSProperty(
-      'font-size',
-    ).value;
-    const expectedDescTextSize = '13.6px';
+    const actualDescTextSize = $(descText).getCSSProperty('font-size').value;
     expect(actualDescTextSize).to.be.equal(expectedDescTextSize);
   });
 
   it('should check font-color', () => {
-    const actualDescTextColor = $('//input[@name="phone"]/following-sibling::small').getCSSProperty(
-      'color',
-    ).parsed.hex;
+    const actualDescTextColor = $(descText).getCSSProperty('color').parsed.hex;
     const expectedDescTextColor = '#6c757d';
     expect(actualDescTextColor).to.be.equal(expectedDescTextColor);
   });
 
   it('should check font-weight', () => {
-    const actualDescTextWeight = $(
-      '//input[@name="phone"]/following-sibling::small',
-    ).getCSSProperty('font-weight').value;
-    const expectedDescTextWeight = 400;
-    expect(actualDescTextWeight).to.be.equal(expectedDescTextWeight);
+    const actualDescTextWeight = $(descText).getCSSProperty('font-weight').value;
+    expect(actualDescTextWeight).to.be.equal(expectedWeight);
   });
 
   it('should check text-alignment', () => {
-    const actualDescTextAlign = $('//input[@name="phone"]/following-sibling::small').getCSSProperty(
-      'text-align',
-    ).value;
-    const expectedDescTextAlign = 'left';
-    expect(actualDescTextAlign).to.be.equal(expectedDescTextAlign);
+    const actualDescTextAlign = $(descText).getCSSProperty('text-align').value;
+    expect(actualDescTextAlign).to.be.equal(expectedAlign);
   });
 });
