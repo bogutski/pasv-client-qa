f9f2c3f50f341c76fc184bb57ca86382f6bf2f33
Irina Williams <ira6170724@gmail.com>
2019-10-01 12:17:45 -0500

    fixed file .eslintrc.js and code in loginUserPasswordDesign.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     'mocha/max-top-level-suites': 0,
 
     indent: ['error', 2],
-    'linebreak-style': 0,
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
   },
--- test/specs/login/loginUserPasswordDesign.js
+++ test/specs/login/loginUserPasswordDesign.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { url } from '../examples/constants';
 
 const inputFieldPassword = '//input[@name="password"]';
 const expectBorderColor = '#ced4da';
@@ -6,7 +7,7 @@ const expectFocusHighlight = 'rgba(0,82,204,0.25)0px0px0px3.2px';
 
 describe('Login Page - Password input field - Design', () => {
   before(() => {
-    browser.url('https://stage.pasv.us/user/login');
+    browser.url(url.loginUrl);
   });
 
   it('should check that the input field is displayed', () => {
@@ -14,12 +15,12 @@ describe('Login Page - Password input field - Design', () => {
     expect(actualInputFieldPassword).to.be.true;
   });
 
-  it('Should "Password" input field has a correct border color', function() {
+  it('Should "Password" input field has a correct border color', () => {
     const actualBorderColor = $(inputFieldPassword).getCSSProperty('border-color').parsed.hex;
     expect(actualBorderColor).to.eq(expectBorderColor);
   });
 
-  it('Should "Password" input field has a correct Focus highlight when user puts a cursor', function() {
+  it('Should "Password" input field has a correct Focus highlight when user puts a cursor', () => {
     $(inputFieldPassword).click(); //can't cath highlight without click and pause
     browser.pause(300);
     const actualFocusHighlight = $(inputFieldPassword).getCSSProperty('box-shadow').value;
