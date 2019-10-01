9aba94c3f2b7b4f77826dd286c1e7f831ccd38ba
RuslanRamazanov72 <omto72@gmail.com>
2019-09-30 21:15:57 -0400

    Chenged userGetAdmin file

--- test/specs/examples/user/actions/userGetAdminToken.js
+++ test/specs/examples/user/actions/userGetAdminToken.js
@@ -2,17 +2,16 @@ import { url } from '../../constants';
 import { user } from '../constants';
 import axios from 'axios/index';
 
-export function userLoginAdmin() {
+export function userGetAdminToken() {
   return axios
     .post(`${url.serverUrl}/user/login`, {
       email: user.admin.email,
       password: user.admin.password,
     })
     .then(response => {
-      const adminToken = response.data.token;
-      console.log(adminToken);
+      return response.data.token;
     })
-    .catch(error => {
-      console.log('------------' + error + '------------');
+    .catch(() => {
+      return null;
     });
 }
