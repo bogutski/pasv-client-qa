908d0218387a807ad5985810878d95f2fc410824
Iurii Sokolov <isokolov@isokolov-macbookpro.roam.corp.google.com>
2019-10-01 12:24:51 -0700

    Adding tests for email field functionality

--- test/specs/registration/emailFunc.js
+++ test/specs/registration/emailFunc.js
@@ -1,8 +1,5 @@
-const { expect } = require('chai');
-const { url } = require('../../../constants');
-
-//import { user } from '../../constants';
-//import userDeleteByEmail from '../../actions/deleteByEmail';
+import { expect } from 'chai';
+import { url } from '../examples/constants';
 
 const elements = {
   emailField: {
@@ -17,6 +14,16 @@ const elements = {
   },
   passwordField: {
     selector: '//input[@name="password"]'
+  },
+  inputValue: {
+    validEmail: '123@test.test',
+    invalidEmail: '123test.test',
+    validEmailAllNumbers: '1234567890@test.test',
+    validEmailAllLetters: 'abcdefghijklmnopqrstuvwxyz@test.test',
+    validEmailSpecialCharacters: '_+%-.@test.test',
+    validEmailAllNumbersEnd: '123@1234567890.test',
+    validEmailAllLettersEnd: '123@abcdefghijklmnopqrstuvwxyz.test'
+
   }
 };
 
@@ -27,26 +34,59 @@ describe('User registration form email functionality', () => {
 
   it('Verify that entered email can contain "@" and "." symbols', () => {
     const emailFieldElement = $(elements.emailField.selector);
-    const passwordFieldElement = $(elements.passwordField.selector)
-    emailFieldElement.setValue('123@test.test');
+    const passwordFieldElement = $(elements.passwordField.selector);
+    emailFieldElement.setValue(elements.inputValue.validEmail);
     passwordFieldElement.click();
-    browser.pause(1000);
-
-    //Verifying invalid email text is not displayed
-    const errorMessageElement = $(elements.errorMessage.selector).isDisplayed();
-    expect(errorMessageElement).to.be.false; 
-    //Verifying border color is green
-    const borderColor = emailFieldElement.getCSSProperty('border-color').parsed.hex.toLowerCase();
-    expect(borderColor).to.eq(elements.greenInputField.expectedBorderColor);
-    
+    expect(emailFieldElement.getAttribute('class')).includes('is-valid');
   });
-
   
   it('Verify that email field doesnt accept email without @ symbol', () => {
-    const element = $(elements.registerButton.selector);
-    element.click();
-    const h1Text = $('//h1').getText();
-    expect(h1Text).eq(elements.registerForm.title);
+    const emailFieldElement = $(elements.emailField.selector);
+    const passwordFieldElement = $(elements.passwordField.selector);
+    emailFieldElement.setValue(elements.inputValue.invalidEmail);
+    passwordFieldElement.click();
+    expect(emailFieldElement.getAttribute('class')).includes('is-invalid');
   });
 
+  it('Verify that email field accepts all numbers in the part BEFORE @', () => {
+    const emailFieldElement = $(elements.emailField.selector);
+    const passwordFieldElement = $(elements.passwordField.selector);
+    emailFieldElement.setValue(elements.inputValue.validEmailAllNumbers);
+    passwordFieldElement.click();
+    expect(emailFieldElement.getAttribute('class')).includes('is-valid');
   });
+
+  it('Verify that email field accepts all letters in the part BEFORE @', () => {
+    const emailFieldElement = $(elements.emailField.selector);
+    const passwordFieldElement = $(elements.passwordField.selector);
+    emailFieldElement.setValue(elements.inputValue.validEmailAllLetters);
+    passwordFieldElement.click();
+    expect(emailFieldElement.getAttribute('class')).includes('is-valid');
+  });
+
+  it('Verify that email field accepts  _, +, %, -, . special characters in the part BEFORE @', () => {
+    const emailFieldElement = $(elements.emailField.selector);
+    const passwordFieldElement = $(elements.passwordField.selector);
+    emailFieldElement.setValue(elements.inputValue.validEmailAllLetters);
+    passwordFieldElement.click();
+    expect(emailFieldElement.getAttribute('class')).includes('is-valid');
+  });
+
+  it('Verify that email field accepts all numbers in the part AFTER @', () => {
+    const emailFieldElement = $(elements.emailField.selector);
+    const passwordFieldElement = $(elements.passwordField.selector);
+    emailFieldElement.setValue(elements.inputValue.validEmailAllNumbersEnd);
+    passwordFieldElement.click();
+    expect(emailFieldElement.getAttribute('class')).includes('is-valid');
+  });
+
+  it('Verify that email field accepts all letters in the part AFTER @', () => {
+    const emailFieldElement = $(elements.emailField.selector);
+    const passwordFieldElement = $(elements.passwordField.selector);
+    emailFieldElement.setValue(elements.inputValue.validEmailAllLettersEnd);
+    passwordFieldElement.click();
+    expect(emailFieldElement.getAttribute('class')).includes('is-valid');
+  });
+  
+
+});
