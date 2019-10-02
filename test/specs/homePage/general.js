c6196892c887bee44ea314a79ae35a9d55843cf1
Larisa <larisaplantation@gmail.com>
2019-10-01 22:13:38 -0400

    [Larisa] changed test HomePage - General

--- test/specs/homePage/general.js
+++ test/specs/homePage/general.js
@@ -5,6 +5,13 @@ const favicon = '//link[@href="/favicon.ico"]';
 const globalHeader = '//div[@id="main-bar"]';
 const header = '//h1';
 const footer = '//footer[@class="pt-5 pb-5"]';
+const images = '//img';
+const textBlocks = '//p[@class="intro"]';
+
+const expectedTitle = 'Progress Monitor';
+const expectedH1 = 'System that considers\nindividual features of\neach student';
+const expectedImgCount = 7;
+const expectedBlockCount = 7;
 
 describe('Home page - General - Design', () => {
   before(() => {
@@ -23,7 +30,6 @@ describe('Home page - General - Design', () => {
 
   it('should verify that home page has correct title', () => {
     const actualTitle = browser.getTitle();
-    const expectedTitle = 'Progress Monitor';
     expect(actualTitle).to.equal(expectedTitle);
   });
 
@@ -41,19 +47,16 @@ describe('Home page - General - Design', () => {
 
   it('should verify that home page contains correct h1', () => {
     const h1Actual = $(header).getText();
-    const h1Expected = 'System that considers\nindividual features of\neach student';
-    expect(h1Actual).equal(h1Expected);
+    expect(h1Actual).to.equal(expectedH1);
   });
 
-  it('should verify that home page contains 7 images', () => {
-    const actualImgCount = $$('//img').length;
-    const expectedImgCount = 7;
+  it('should verify that home page contains correct number of images', () => {
+    const actualImgCount = $$(images).length;
     expect(actualImgCount).to.equal(expectedImgCount);
   });
 
-  it('should verify that home page contains 7 text blocks', () => {
-    const actualBlockCount = $$('//p[@class="intro"]').length;
-    const expectedBlockCount = 7;
+  it('should verify that home page contains correct number of text blocks', () => {
+    const actualBlockCount = $$(textBlocks).length;
     expect(actualBlockCount).to.equal(expectedBlockCount);
   });
 
