4e3346ccf3721df34067cf7ef3e3813a12bf9a5d
Katsiaryna Aulaska <52294376+KatsiarynaA@users.noreply.github.com>
2019-10-06 19:08:23 -0400

    Add "User register error message design" test. (#43)
    * Add "User register error message design" test.
    
    * Split "name" into "firstName" and "lastName".
    
    * Use registerAction in "errorMessageDesign" file.

--- test/examples/user/constants.js
+++ test/examples/user/constants.js
@@ -3,16 +3,19 @@ export const user = {
     email: 'admin@test.com',
     password: '11111',
     id: '5d687e4983d5d600380e6b51',
-    name: 'Ruslan Admin',
+    firstName: 'Ruslan',
+    lastName: 'Admin',
     phone: '12312312334',
   },
   student: {
     email: 'student_viktor@test.com',
     password: '11111',
     // id: '5d687e4983d5d600380e6b51',
-    name: 'Test Student',
+    firstName: 'Test',
+    lastName: 'Student',
     phone: '12312312334',
     about: 'About Test Student About Test Student About Test Student About Test Student',
     goals: 'About Test Student About Test Student About Test Student About Test Student',
+    englishLevel: 'Elementary'
   },
 };
