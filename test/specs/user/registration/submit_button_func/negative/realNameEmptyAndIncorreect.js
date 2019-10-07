f08db55b566b9e1d7bf7c429d2b85fb50172791b
RuslanRamazanov72 <omto72@gmail.com>
2019-10-06 22:24:52 -0400

    Added changes to realNameEmptyandIncorrect file, fixed bugs

--- test/specs/user/registration/submit_button_func/negative/realNameEmptyAndIncorreect.js
+++ test/specs/user/registration/submit_button_func/negative/realNameEmptyAndIncorreect.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 import { url } from '../../../../constants';
 import { user } from '../../../../../examples/user/constants';
 
-describe('REGISTRATION FORM SUBMIT BUTTON SHOULD DISABLED WHEN REAL NAME IS EMPTY', () => {
+describe('User - Register - Submit button - Disabled - First name empty', () => {
   before(() => {
     browser.url(url.register);
     const cellPhoneNumberField = $('//input[@name="phone"]');
@@ -20,17 +20,17 @@ describe('REGISTRATION FORM SUBMIT BUTTON SHOULD DISABLED WHEN REAL NAME IS EMPT
     englishLevelField.selectByVisibleText('Native');
   });
 
-  it('should check that Real Name field is empty', () => {
-    const realNameField = $('//input[@name="name"]');
-    realNameField.click();
+  it('should check that First name field is empty', () => {
+    const firstNameField = $('//input[@name="firstName"]');
+    firstNameField.click();
     browser.keys('Tab');
     browser.pause(1000);
-    const actualBorderColor = realNameField.getCSSProperty('border-color').parsed.hex;
+    const actualBorderColor = firstNameField.getCSSProperty('border-color').parsed.hex;
     const expectedBorderColor = '#ff4465';
 
     expect(actualBorderColor).equal(expectedBorderColor);
   });
-  it('should check that submit button is disabled when Real Name is empty', () => {
+  it('should check that submit button is disabled when first name field is empty', () => {
     const submitButton = $('//button[contains(@class,"btn btn-primary disabled")]');
     const actualResult = submitButton.isEnabled();
 
@@ -38,10 +38,10 @@ describe('REGISTRATION FORM SUBMIT BUTTON SHOULD DISABLED WHEN REAL NAME IS EMPT
   });
 });
 
-describe('REGISTRATION FORM SUBMIT BUTTON SHOULD DISABLED WHEN REAL NAME FILLED INCORRECT', () => {
+describe('User - Register - Submit button - Disabled - First name incorrect', () => {
   before(() => {
     browser.url(url.register);
-
+    const lastName = $('//input[@name="lastName"]');
     const cellPhoneNumberField = $('//input[@name="phone"]');
     const emailField = $('//input[@name="email"]');
     const passwordField = $("//input[@name='password']");
@@ -49,6 +49,7 @@ describe('REGISTRATION FORM SUBMIT BUTTON SHOULD DISABLED WHEN REAL NAME FILLED 
     const goalsField = $('//textarea[@name="goals"]');
     const englishLevelField = $('//select[contains(@class,"form-control")]');
 
+    lastName.setValue(user.student.lastName);
     cellPhoneNumberField.setValue(user.student.phone);
     emailField.setValue(user.student.email);
     passwordField.setValue(user.student.password);
@@ -57,8 +58,8 @@ describe('REGISTRATION FORM SUBMIT BUTTON SHOULD DISABLED WHEN REAL NAME FILLED 
     englishLevelField.selectByVisibleText('Native');
   });
 
-  it('should check that real name filled incorrect', () => {
-    const realNameField = $('//input[@name="name"]');
+  it('should check that first name filled incorrect', () => {
+    const firstNameField = $('//input[@name="firstName"]');
     const arrNames = [
       'J Jhonson',
       'jo smith',
@@ -67,20 +68,20 @@ describe('REGISTRATION FORM SUBMIT BUTTON SHOULD DISABLED WHEN REAL NAME FILLED 
       '12566',
       ' ',
       'Ruslan ',
-      'Jhon',
       '&&*^^$@',
     ];
     for (let i = 0; i < arrNames.length; i++) {
-      realNameField.setValue(arrNames[i]);
+      firstNameField.setValue(arrNames[i]);
       browser.keys('Tab');
       browser.pause(1000);
-      const actualBorderColor = realNameField.getCSSProperty('border-color').parsed.hex;
+      const actualBorderColor = firstNameField.getCSSProperty('border-color').parsed.hex;
       const expectedBorderColor = '#ff4465';
+
       expect(actualBorderColor).equal(expectedBorderColor);
     }
   });
 
-  it('should check that submit button disabled when Real Name filled incorrect', () => {
+  it('should check that submit button disabled when first Name filled incorrect', () => {
     const submitButton = $('//button[contains(@class,"btn btn-primary disabled")]');
     const actualResult = submitButton.isEnabled();
 
