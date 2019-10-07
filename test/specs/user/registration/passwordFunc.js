82bcdc9e4322f2dbfa32c4f5d913481578fa2573
SergeiLatyshev <sergei.new.2016@gmail.com>
2019-10-07 15:15:39 -0400

    /user:register:passwordFunc test

---
+++ test/specs/user/registration/passwordFunc.js
@@ -0,0 +1,165 @@
+import { expect } from 'chai';
+import { url } from '../../../specs/constants';
+
+const registrationFields = {
+  firstName: '//input[@name="firstName"]',
+  lastName: '//input[@name="lastName"]',
+  phone: '//input[@name="phone"]',
+  email: '//input[@name="email"]',
+  password: '//input[@name="password"]',
+  about: '//textarea[@name="about"]',
+  goals: '//textarea[@name="goals"]',
+  english: '//select[contains(@class,"form-control")]',
+  button: '//button[text()="Submit"]',
+};
+
+describe('Password functionality', () => {
+  before(() => {
+    browser.url(url.register);
+    browser.maximizeWindow();
+  });
+
+  it('should enter first Name', () => {
+    const field = $(registrationFields.firstName);
+    field.setValue('Sergei');
+  });
+
+  it('should enter last Name', () => {
+    const field = $(registrationFields.lastName);
+    field.setValue('Latyshev');
+  });
+
+  it('should enter Cellphone', () => {
+    const field = $(registrationFields.phone);
+    field.setValue('15557779999');
+  });
+
+  it('should enter Email', () => {
+    const field = $(registrationFields.email);
+    field.setValue('latyshev@test.com');
+  });
+
+  it('should enter About', () => {
+    const field = $(registrationFields.about);
+    field.setValue('test test test');
+  });
+
+  it('should enter My Goals', () => {
+    const field = $(registrationFields.goals);
+    field.setValue('test test test');
+  });
+
+  it('should choose English Level', () => {
+    const field = $(registrationFields.english);
+    field.selectByVisibleText('Elementary');
+  });
+
+  it('should verify that "Password field" is required', () => {
+    const button = $(registrationFields.button);
+    const actual = button.isEnabled();
+    expect(actual).to.be.false;
+  });
+
+  it('should check "Password field" with correct password', () => {
+    const field = $(registrationFields.password);
+    field.setValue('1234567890abcde');
+    browser.keys('Tab');
+  });
+
+  it('should check that all symbols in the "Password field" are covered with bullets ', () => {
+    const field = $(registrationFields.password);
+    const bullets = field.getCSSProperty('-webkit-text-security').value;
+    expect(bullets).to.be.equal('disc');
+  });
+
+  it('should allow to submit with correct password', () => {
+    const button = $(registrationFields.button);
+    const actual = button.isEnabled();
+    expect(actual).to.be.true;
+  });
+
+  it('should mark field as valid with correct password', () => {
+    const actual = $(registrationFields.password).getAttribute('class');
+    const expected = 'is-valid';
+    expect(actual).to.contains(expected);
+  });
+
+  it('should check "Password field" with short password', () => {
+    const field = $(registrationFields.password);
+    field.setValue('123a');
+    browser.keys('Tab');
+  });
+
+  it('should not allow to submit with short password', () => {
+    const button = $(registrationFields.button);
+    const actual = button.isEnabled();
+    expect(actual).to.be.false;
+  });
+
+  it('should display error message for short password', () => {
+    const message = $("//div[text()='Must be 5 characters or more']");
+    const actual = message.isDisplayed();
+    expect(actual).to.be.true;
+  });
+
+  it('should mark field as invalid with short password', () => {
+    const actual = $(registrationFields.password).getAttribute('class');
+    const expected = 'is-invalid';
+    expect(actual).to.contains(expected);
+  });
+
+  it('should check "Password field" with long password', () => {
+    const field = $(registrationFields.password);
+    field.setValue('1234567890abcde1234567890abcde1');
+    browser.keys('Tab');
+  });
+
+  it('should not allow to submit with long password', () => {
+    const button = $(registrationFields.button);
+    const actual = button.isEnabled();
+    expect(actual).to.be.false;
+  });
+
+  it('should displayed error message for long password', () => {
+    const message = $("//div[text()='Must be 30 characters or less']");
+    const actual = message.isDisplayed();
+    expect(actual).to.be.true;
+  });
+
+  it('should mark field as invalid with long password', () => {
+    const actual = $(registrationFields.password).getAttribute('class');
+    const expected = 'is-invalid';
+    expect(actual).to.contains(expected);
+  });
+
+  it('should should check "Password field" take any symbols in password', () => {
+    const field = $(registrationFields.password);
+    field.setValue('1Aa@#$%^&*()_+\\|/<>=');
+  });
+
+  it('should allow to submit with any symbols in password', () => {
+    const button = $(registrationFields.button);
+    const actual = button.isEnabled();
+    expect(actual).to.be.true;
+  });
+
+  it('should mark field as valid with any symbols in password', () => {
+    const actual = $(registrationFields.password).getAttribute('class');
+    const expected = 'is-valid';
+    expect(actual).to.contains(expected);
+  });
+
+  it('should should check "Password field" is required if it was cleared', () => {
+    const field = $(registrationFields.password);
+    field.clearValue();
+    const button = $(registrationFields.button);
+    const actual = button.isEnabled();
+    expect(actual).to.be.false;
+  });
+
+  it('should mark field as invalid with if it was cleared', () => {
+    const actual = $(registrationFields.password).getAttribute('class');
+    const expected = 'is-invalid';
+    expect(actual).to.contains(expected);
+  });
+});
