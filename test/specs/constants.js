25ab1631c6a391b9fee6333b2a36e332628feea1
Larisa <larisaplantation@gmail.com>
2019-10-10 23:21:02 -0400

    change in constants after bug fixing

--- test/specs/constants.js
+++ test/specs/constants.js
@@ -12,7 +12,6 @@ const url = {
   diaryCreateForm: `${baseUrl}/diary/create`,
 
   group: `${baseUrl}/group`,
-  //groupCreated: `${baseUrl}/groups`,
   createGroup: `${baseUrl}/group/create`,
 
   server: 'https://server-stage.pasv.us',
