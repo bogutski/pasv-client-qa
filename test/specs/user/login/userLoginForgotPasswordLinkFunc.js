07219969b1666d9d8a5ede4965079b83f7d510a6
Vladyslavva <vladyslava.iurchenko@gmail.com>
2019-10-12 16:44:55 -0700

    Fixed and made changes according to requests

--- test/specs/user/login/userLoginForgotPasswordLinkFunc.js
+++ test/specs/user/login/userLoginForgotPasswordLinkFunc.js
@@ -1,13 +1,18 @@
 import { url } from '../../constants.js';
 import { expect } from 'chai';
-const forgotPasswordLinkSelector = '//a[@qa="forgot-password-link"]';
-const expectedh1ForgotPasswordPage = 'Reset your password';
-const enterYourEmailFieldSelector = '//input[@name="email"]';
+
+const selectors = {
+  forgotPasswordLink: '//a[@qa="forgot-password-link"]',
+  enterYourEmailField: '//input[@name="email"]',
+  button: '//button[@type="submit"]',
+  failMessage: '//h4[contains(text(),"User not found")]',
+  h1: '//h1',
+};
+
 const validEmail = 'admin@test.com';
 const invalidEmail = 'goa@gmail.com';
-const buttonSelector = '//button[@type="submit"]';
+const expectedh1ForgotPasswordPage = 'Reset your password';
 const expectedh1SuccessMessage = 'Check your email for a link to reset your password';
-const failMessageSelector = '//h4[contains(text(),"User not found")]';
 const expectedfailMessage = 'User not found';
 
 describe('User - Login - Forgot Password Link - Func', () => {
@@ -15,44 +20,48 @@ describe('User - Login - Forgot Password Link - Func', () => {
     browser.url(url.login);
   });
 
-  it('Should verify that  Forgot password link is active', () => {
-    $(forgotPasswordLinkSelector).click();
-    const h1ForgotPasswordPage = $('//h1').getText();
+  it('Should verify that Forgot password link is displayed', () => {
+    expect($(selectors.forgotPasswordLink).isDisplayed()).to.be.true;
+  });
+
+  it('Should verify that Forgot password link is active', () => {
+    $(selectors.forgotPasswordLink).click();
+    const h1ForgotPasswordPage = $(selectors.h1).getText();
     expect(h1ForgotPasswordPage).eq(expectedh1ForgotPasswordPage);
   });
 
   it('Should verify that Password reset email button is inactive', () => {
-    const buttonIsEnabled = $(buttonSelector).isEnabled();
+    const buttonIsEnabled = $(selectors.button).isEnabled();
     expect(buttonIsEnabled).to.be.false;
   });
 
-  it('Should verify that Enter your email address field can be filled', () => {
-    $(enterYourEmailFieldSelector).click();
-    $(enterYourEmailFieldSelector).setValue(validEmail);
-    const expectedText = $(enterYourEmailFieldSelector).getValue();
+  it('Should verify that "Enter your email address" text field can be filled', () => {
+    $(selectors.enterYourEmailField).click();
+    $(selectors.enterYourEmailField).setValue(validEmail);
+    const expectedText = $(selectors.enterYourEmailField).getValue();
     expect(validEmail).to.eq(expectedText);
   });
 
-  it('Should verify that Password reset email button is active', () => {
-    const buttonIsEnabled = $(buttonSelector).isEnabled();
+  it('Should verify that Password reset email button is active, when valid email is entered', () => {
+    const buttonIsEnabled = $(selectors.button).isEnabled();
     expect(buttonIsEnabled).to.be.true;
   });
 
-  it('Should verify the Auth failed. User not found message', () => {
-    $(enterYourEmailFieldSelector).click();
-    $(enterYourEmailFieldSelector).setValue(invalidEmail);
-    $(buttonSelector).click();
+  it('Should verify the Auth failed. "User not found" message is displayed', () => {
+    $(selectors.enterYourEmailField).click();
+    $(selectors.enterYourEmailField).setValue(invalidEmail);
+    $(selectors.button).click();
     browser.pause(1500);
-    const failMessage = $(failMessageSelector).getText();
+    const failMessage = $(selectors.failMessage).getText();
     expect(failMessage).eq(expectedfailMessage);
   });
 
   it('Should verify the Auth success message', () => {
-    $(enterYourEmailFieldSelector).click();
-    $(enterYourEmailFieldSelector).setValue(validEmail);
-    $(buttonSelector).click();
+    $(selectors.enterYourEmailField).click();
+    $(selectors.enterYourEmailField).setValue(validEmail);
+    $(selectors.button).click();
     browser.pause(2000);
-    const h1SuccessMessage = $('//h1').getText();
+    const h1SuccessMessage = $(selectors.h1).getText();
     expect(h1SuccessMessage).eq(expectedh1SuccessMessage);
   });
 });
