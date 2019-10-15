ff2412c53ed0fb797cca41a7155a61b73ba6f4aa
kate <klipsakate@gmail.com>
2019-10-15 13:14:20 -0400

    Refactoring "errorMessageDesign.js" file

--- test/specs/user/registration/errorMessageDesign.js
+++ test/specs/user/registration/errorMessageDesign.js
@@ -6,6 +6,17 @@ import { user } from './../_data/data';
 const token = process.env.TOKEN_ADMIN;
 const wrapperErrorMessage = '//div[contains(@class,"notification-error")]';
 const errorMessage = `${wrapperErrorMessage}/h4`;
+const errorMessageText = 'e-mail exists';
+const cssPropertyFontSize = 'font-size';
+const cssPropertyFontWeight = 'font-weight';
+const cssPropertyFontColor = 'color';
+const cssPropertyTextAlignment = 'text-align';
+const cssPropertyFontFamily = 'font-family';
+const cssPropertyBackgroundColor = 'background-color';
+const cssPropertyBorderTopWidth = 'border-top-width';
+const cssPropertyBorderTopStyle = 'border-top-style';
+const cssPropertyBorderTopColor = 'border-top-color';
+const cssPropertyBoxShadow = 'box-shadow';
 const expected = {
   fontSize: '14px',
   fontWeight: '700',
@@ -18,10 +29,14 @@ const expected = {
   borderTopColor: '#ec3d3d',
   boxShadow: '#ec3d3d',
 };
+let allUsers;
 
 describe('User - Registration - ErrorMessage - Design', () => {
   before(async () => {
-    const allUsers = await userGetAll(token);
+    allUsers = await userGetAll(token);
+  });
+
+  it('should register the user if he is not in the database', () => {
     if (!allUsers.some(existingUser => existingUser.email === user.student.email)) {
       registerAction(browser);
     }
@@ -43,75 +58,75 @@ describe('User - Registration - ErrorMessage - Design', () => {
 
   it('should have the correct text', () => {
     const actualText = $(errorMessage).getText();
-    expect(actualText).includes('e-mail exists');
+    expect(actualText).includes(errorMessageText);
   });
 
   it('should have the correct font-size', () => {
     const actualFontSize = $(errorMessage)
-      .getCSSProperty('font-size')
+      .getCSSProperty(cssPropertyFontSize)
       .parsed.string;
     expect(actualFontSize).to.be.equal(expected.fontSize);
   });
 
   it('should have the correct font-weight', () => {
     const actualFontWeight = $(errorMessage)
-      .getCSSProperty('font-weight')
+      .getCSSProperty(cssPropertyFontWeight)
       .parsed.string;
     expect(actualFontWeight).to.be.equal(expected.fontWeight);
   });
 
   it('should have the correct font-color', () => {
     const actualFontColor = $(errorMessage)
-      .getCSSProperty('color')
+      .getCSSProperty(cssPropertyFontColor)
       .parsed.hex.toLowerCase();
     expect(actualFontColor).to.be.equal(expected.fontColor);
   });
 
   it('should have the correct text-alignment', () => {
     const actualTextAlignment = $(errorMessage)
-      .getCSSProperty('text-align')
+      .getCSSProperty(cssPropertyTextAlignment)
       .parsed.string;
     expect(actualTextAlignment).to.be.equal(expected.textAlignment);
   });
 
   it('should have the correct font-family', () => {
     const actualFontFamily = $(errorMessage)
-      .getCSSProperty('font-family')
+      .getCSSProperty(cssPropertyFontFamily)
       .parsed.string.toLowerCase();
     expect(actualFontFamily).to.be.equal(expected.fontFamily);
   });
 
   it('should have the correct background-color', () => {
     const actualBackgroundColor = $(wrapperErrorMessage)
-      .getCSSProperty('background-color')
+      .getCSSProperty(cssPropertyBackgroundColor)
       .parsed.hex.toLowerCase();
     expect(actualBackgroundColor).to.be.equal(expected.backgroundColor);
   });
 
   it('should have the correct border-top-width', () => {
     const actualBorderTopWidth = $(wrapperErrorMessage)
-      .getCSSProperty('border-top-width')
+      .getCSSProperty(cssPropertyBorderTopWidth)
       .parsed.string;
     expect(actualBorderTopWidth).to.be.equal(expected.borderTopWidth);
   });
 
   it('should have the correct border-top-style:', () => {
     const actualBorderTopStyle = $(wrapperErrorMessage)
-      .getCSSProperty('border-top-style')
+      .getCSSProperty(cssPropertyBorderTopStyle)
       .parsed.string;
     expect(actualBorderTopStyle).to.be.equal(expected.borderTopStyle);
   });
 
   it('should have the correct border-top-color:', () => {
     const actualBorderTopColor = $(wrapperErrorMessage)
-      .getCSSProperty('border-top-color')
+      .getCSSProperty(cssPropertyBorderTopColor)
       .parsed.hex.toLowerCase();
     expect(actualBorderTopColor).to.be.equal(expected.borderTopColor);
   });
 
   it('should have the correct box-shadow:', () => {
     const actualBoxShadow = $(wrapperErrorMessage)
-      .getCSSProperty('box-shadow')
+      .getCSSProperty(cssPropertyBoxShadow)
       .parsed.hex.toLowerCase();
     expect(actualBoxShadow).to.be.equal(expected.boxShadow);
   });
