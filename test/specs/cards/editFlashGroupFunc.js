67218dc83150ac5277e05f6fdf803342f4bf4d72
Larisa <larisaplantation@gmail.com>
2019-10-19 22:36:40 -0400

    added test to editFlashGroupFunc

---
+++ test/specs/cards/editFlashGroupFunc.js
@@ -0,0 +1,67 @@
+import { expect } from 'chai';
+import { url } from './../constants';
+import loginAction from './../user/_actions/loginAction';
+
+const selector = {
+  menuCards: '//li/a[@qa="cards-link"]', //+
+  h1: '//h1',
+  flashGroupName: '//h4[@qa="name"]', //+
+  groupNameField: '//input[@name="name"]', //+
+  groupDescriptionField: '//input[@name="description"]', //+
+  groupDescription: '//div[@qa="description"]', //+
+  //  createButton: '//button[@class="btn btn-primary"]',
+  saveButton: '//button[@class="btn btn-primary"]', //+
+  editButton: '//a[@class="edit"]', //+
+};
+
+const expected = {
+  h1Cards: 'FlashCards',
+  h1EditGroup: 'Edit ',
+};
+
+const data = {
+  addToName: '_test', //+
+  addToDescription: '_test_questions', //+
+};
+
+let flashGroupNameOld;
+let flashGroupDescriptionOld;
+
+describe('Cards - Edit FlashCardGroup - Functionality', () => {
+  before(() => {
+    loginAction(browser);
+  });
+
+  it('should check name of last flash group on `FlashCards` page', () => {
+    $(selector.menuCards).click();
+    flashGroupNameOld = $$(selector.flashGroupName)[0].getText();
+    expect(flashGroupNameOld.length > 0).to.be.true;
+  });
+
+  it('should check description of last flash group on `FlashCards` page', () => {
+    $(selector.menuCards).click();
+    flashGroupDescriptionOld = $$(selector.groupDescription)[0].getText();
+    expect(flashGroupDescriptionOld.length > 0).to.be.true;
+  });
+
+  it('should verify h1 after clicking to `edit` button redirect to `Edit Group` page ', () => {
+    $(selector.editButton).click();
+    browser.pause(6000);
+    const actualH1 = $(selector.h1).getText();
+    expect(actualH1).equal(expected.h1EditGroup + flashGroupNameOld);
+  });
+
+  it('should verify URL that after editing flashGroup and clicking Save button', () => {
+    $(selector.groupNameField).addValue(data.addToName);
+    $(selector.groupDescriptionField).addValue(data.addToDescription);
+    $(selector.saveButton).click();
+    browser.pause(1000);
+    const actualUrl = browser.getUrl();
+    expect(actualUrl).equal(url.card);
+  });
+
+  it('should verify H1 after editing flashGroup and redirecting to `FlashCards` page ', () => {
+    const actualH1 = $(selector.h1).getText();
+    expect(actualH1).equal(expected.h1Cards);
+  });
+});
