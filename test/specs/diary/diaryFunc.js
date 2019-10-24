5771baabb42bbdaa88b5d8d9b4b1156c9de2cdb7
Larisa <larisaplantation@gmail.com>
2019-10-24 18:05:22 -0400

    added test to dairyFunc

--- test/specs/diary/diaryFunc.js
+++ test/specs/diary/diaryFunc.js
@@ -13,6 +13,7 @@ const selector = {
   morale: '//select[@name="morale"]',
   hours: '//input[@name="hours"]',
   descriptionField: '//textarea[@name="description"]',
+  invalidFeedback: '//div[@class="invalid-feedback"]',
 };
 
 const diaryH1 = 'Daily reports';
@@ -21,11 +22,13 @@ const dayReportShortText = 'Today I wrote tests.';
 const dayReportText = `Today I watched ${Math.trunc(
   Math.random() * 10,
 )} lectures and solved ${Math.trunc(Math.random() * 10)} tasks on codewars. Also I wrote tests.`;
+const morale = Math.trunc(Math.random() * 10);
+const hours = Math.trunc(Math.random() * 10);
+const incorrectHours = 11;
 
 let allDiaries;
 let initialDiaryCount;
 let countOfCheckBoxes;
-let morale = Math.trunc(Math.random() * 10);
 
 describe('Diary - Functionality', () => {
   before(() => {
@@ -95,34 +98,32 @@ describe('Diary - Functionality', () => {
     expect(isEnabled).to.be.false;
   });
 
-  it('should verify that `Save` button is disabled when there is the day report, but there are no checkboxes', () => {
+  it('should verify that checkboxes are required (`Save` button is disabled when there are no checkboxes)', () => {
     const descriptionArea = $(selector.descriptionField);
     descriptionArea.setValue(dayReportText);
     const isEnabled = $(selector.saveButton).isEnabled();
     expect(isEnabled).to.be.false;
   });
 
-  it('should verify that `Save` button is disabled when there are checkboxes, but there is no day report', () => {
+  it('should check all checkboxes', () => {
     for (let i = 0; i < countOfCheckBoxes; i++) {
       const selector = $('//input[@id="input-[' + i + ']"]');
       selector.click();
     }
-    const descriptionArea = $(selector.descriptionField);
-    descriptionArea.setValue('');
-    const isEnabled = $(selector.saveButton).isEnabled();
-    expect(isEnabled).to.be.false;
-  });
-
-  it('should verify that `Save` button is disabled when there are checkboxes, but day report is < 30 characters', () => {
-    const descriptionArea = $(selector.descriptionField);
-    descriptionArea.setValue(dayReportShortText);
     const isEnabled = $(selector.saveButton).isEnabled();
     expect(isEnabled).to.be.false;
   });
 
   it('should verify that description area is-invalid when day report length < 30', () => {
     const descriptionArea = $(selector.descriptionField);
+    descriptionArea.setValue(dayReportShortText);
     expect(descriptionArea.getAttribute('class')).includes('is-invalid');
+  });
+
+  it('should verify that description area is-valid when daily report text >= 30 characters', () => {
+    const descriptionArea = $(selector.descriptionField);
+    descriptionArea.setValue(dayReportText);
+    expect(descriptionArea.getAttribute('class')).includes('is-valid');
   });
 
   it('should verify that `Morale` is required field (is-invalid when it is empty)', () => {
@@ -136,19 +137,41 @@ describe('Diary - Functionality', () => {
     const moraleField = $(selector.morale);
     moraleField.selectByVisibleText(morale);
     browser.keys('Tab');
+    browser.pause(600);
     expect(moraleField.getAttribute('class')).includes('is-valid');
   });
 
-  it('should verify that `Save` button is enabled when there is a correct report and checkboxes', () => {
-    const descriptionArea = $(selector.descriptionField);
-    descriptionArea.setValue(dayReportText);
-    const isEnabled = $(selector.saveButton).isEnabled();
-    expect(isEnabled).to.be.true;
+  it('should verify that `Hours` is required field (is-invalid when it is empty)', () => {
+    const hoursField = $(selector.hours);
+    hoursField.setValue('');
+    browser.keys('Tab');
+    expect(hoursField.getAttribute('class')).includes('is-invalid ');
   });
 
-  it('should verify that description area is-valid when day report length >= 30', () => {
-    const descriptionArea = $(selector.descriptionField);
-    expect(descriptionArea.getAttribute('class')).includes('is-valid');
+  it('should verify that maximum hours in `Hours` field is 10 (is-invalid when enter 11)', () => {
+    const hoursField = $(selector.hours);
+    hoursField.setValue(incorrectHours);
+    browser.keys('Tab');
+    expect(hoursField.getAttribute('class')).includes('is-invalid');
+  });
+
+  it('should verify that if hours>10 then invalid feedback is appeared)', () => {
+    const hoursField = $(selector.hours);
+    hoursField.setValue(incorrectHours);
+    browser.keys('Tab');
+    expect($(selector.invalidFeedback).isDisplayed()).to.be.true;
+  });
+
+  it('should verify that `Hours` is required field (is-valid when it is not empty)', () => {
+    const hoursField = $(selector.hours);
+    hoursField.setValue(hours);
+    browser.keys('Tab');
+    expect(hoursField.getAttribute('class')).includes('is-valid');
+  });
+
+  it('should verify that `Save` button is enabled when checkboxes and all required fields are correct', () => {
+    const isEnabled = $(selector.saveButton).isEnabled();
+    expect(isEnabled).to.be.true;
   });
 
   it('should verify that click to `Save` button should redirect to `Day reports` page', () => {
